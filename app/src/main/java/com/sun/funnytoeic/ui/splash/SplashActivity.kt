a744c4a832c741330f063c6e193c7ca22939aea7
PhucSSK <lhphuc.ynwa@gmail.com>
2019-08-06 14:31:12 +0700

    Init project

---
+++ app/src/main/java/com/sun/funnytoeic/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.sun.funnytoeic.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sun.funnytoeic.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
