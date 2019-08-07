cbbd25fb2e22a4b731e87ca0183fbe4c85bafdfb
PhucSSK <lhphuc.ynwa@gmail.com>
2019-08-07 15:20:56 +0700

    Create Splash Screen UI

--- app/src/main/java/com/sun/funnytoeic/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/sun/funnytoeic/ui/base/BaseViewModel.kt
@@ -2,12 +2,24 @@ package com.sun.funnytoeic.ui.base
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.SupervisorJob
 
 abstract class BaseViewModel : ViewModel() {
 
     private val isLoading = MutableLiveData<Boolean>().apply { value = false }
-
     private val errorMessage = MutableLiveData<String>()
+    private val supervisorJob = SupervisorJob()
+
+    protected val mainScope = CoroutineScope(supervisorJob + Main)
+    protected val ioScope = CoroutineScope(supervisorJob + IO)
+
+    override fun onCleared() {
+        super.onCleared()
+        supervisorJob.cancel()
+    }
 
     open fun showError(e: Throwable) {
         errorMessage.value = e.message
